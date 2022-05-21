1246c0f766b244d155fea05ee7446d396b0c4931
imksmv <98290303+imksmv@users.noreply.github.com>
2022-05-21 12:45:34 +0100

    Hero section added 👤

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,18 @@
 import React from "react";
+import "../components/Hero.css";
+import Grid from "../assets/grid.png";
 
 const Hero = () => {
-    return <div></div>;
+    return (
+        <div className="hero">
+            <img className="hero__img" src={Grid} alt="Grid" />
+            <h1 className="hero__title">Online Experiences</h1>
+            <p className="hero__text">
+                Join unique interactive activities led by one-of-a-kind
+                hosts—all without leaving home.
+            </p>
+        </div>
+    );
 };
 
 export default Hero;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "../components/Navbar.css";
 import Logo from "../assets/logo.svg";
 
 const Navbar = () => {
